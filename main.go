157e316a086716ea8b32d2a850e9a7017150dd7d
Nitesh Agrawal <nit.agrawal@sap.com>
2023-03-03 17:14:12 +0530

    changes

--- main.go
+++ main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
-func DumpRequest(w http.ResponseWriter, req *http.Request) {
-	requestDump, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		fmt.Fprint(w, err.Error())
-	} else {
-		fmt.Fprint(w, string(requestDump))
-	}
-}
-
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/dumprequestG", DumpRequest).Methods("GET")
-	router.HandleFunc("/dumprequestP", DumpRequest).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Fprint(w, err.Error())
+		} else {
+			fmt.Fprint(w, string(requestDump))
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
